493a7951605db9bcc9942257c3643e69e1d5fd84
Tomas Coria <coria.tomasg@gmail.com>
2021-03-01 01:43:43 -0300

    Fixed social media icon problem when starting the webpage on desktop

--- main.js
+++ main.js
@@ -2,12 +2,15 @@ const mQuery = window.matchMedia('(min-width: 1024px)');
 
 const social_media = document.getElementById("social_media");
 
-// if (mediaQuery_desktop.matches) {
-//     const size = 3;
+if (mQuery.matches) {
+    let size = 3;
+    social_media.innerHTML = `<li class='social_media-icon'><i class='fab fa-facebook-square fa-${size}x'></i></li><li class='social_media-icon'><i class='fab fa-twitter-square fa-${size}x'></i></li><li class='social_media-icon'><i class='fab fa-instagram fa-${size}x'></i></li><li class='social_media-icon'><i class='fab fa-youtube fa-${size}x'></i></li>`
+}else {
+    let size = 2;
+    social_media.innerHTML = `<li class='social_media-icon'><i class='fab fa-facebook-square fa-${size}x'></i></li><li class='social_media-icon'><i class='fab fa-twitter-square fa-${size}x'></i></li><li class='social_media-icon'><i class='fab fa-instagram fa-${size}x'></i></li><li class='social_media-icon'><i class='fab fa-youtube fa-${size}x'></i></li>`
 
-//     social_media.innerHTML = `<li class='social_media-icon'><i class='fab fa-facebook-square fa-${size}x'></i></li><li class='social_media-icon'><i class='fab fa-twitter-square fa-${size}x'></i></li><li class='social_media-icon'><i class='fab fa-instagram fa-${size}x'></i></li><li class='social_media-icon'><i class='fab fa-youtube fa-${size}x'></i></li>`
 
-// }
+}
 
 function  screenTest(e){
     if(e.matches){
